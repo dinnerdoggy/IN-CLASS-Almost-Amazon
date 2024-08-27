a330d3eaf4dd94685a6646bd9605c34985000a8e
Casey <dinnerdoggy@gmail.com>
2024-08-26 19:47:15 -0500

    can now update books

--- api/bookData.js
+++ api/bookData.js
@@ -29,7 +29,17 @@ const deleteBook = (firebaseKey) => new Promise((resolve, reject) => {
 });
 
 // TODO: GET SINGLE BOOK
-const getSingleBook = () => {};
+const getSingleBook = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/books/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data)) // will resolve a single object
+    .catch(reject);
+});
 
 // TODO: CREATE BOOK
 const createBook = (payload) => new Promise((resolve, reject) => {
--- events/domEvents.js
+++ events/domEvents.js
@@ -1,5 +1,5 @@
 import { getAuthors, deleteSingleAuthor } from '../api/authorData';
-import { getBooks, deleteBook } from '../api/bookData';
+import { getBooks, getSingleBook, deleteBook } from '../api/bookData';
 import addBookForm from '../components/forms/addBookForm';
 import { showAuthors } from '../pages/authors';
 import { showBooks } from '../pages/books';
@@ -26,9 +26,11 @@ const domEvents = () => {
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-      console.warn('EDIT BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
     }
+
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
     if (e.target.id.includes('view-book-btn')) {
       console.warn('VIEW BOOK', e.target.id);
